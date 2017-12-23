1b64fff02b7f49f6c8964d29c79cb87ec7152a9f
Jazib Sawar <jazibsawar@gmail.com>
2017-12-24 01:39:26 +0500

    chore(component): BlogTitle component added. It will change document.title

---
+++ src/components/BlogTitle.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'blog-title',
+  props: ['title'],
+  created () {
+    document.title = this.title;
+  },
+  watch: {
+    title () {
+      document.title = this.title;
+    }
+  },
+  render() {
+  }
+}
+</script>
